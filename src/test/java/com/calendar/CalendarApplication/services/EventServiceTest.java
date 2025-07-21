e2386a59572c2909d921ed5a2dd54a9f9a4785b8
gustavo <gugaxavier2005@gmail.com>
2025-07-21 19:23:52 -0300

    finish events tests

--- src/test/java/com/calendar/CalendarApplication/services/EventServiceTest.java
+++ src/test/java/com/calendar/CalendarApplication/services/EventServiceTest.java
@@ -1,6 +1,7 @@
 package com.calendar.CalendarApplication.services;
 
 import com.calendar.CalendarApplication.dtos.event.CreateEventDto;
+import com.calendar.CalendarApplication.dtos.event.GetEventsDto;
 import com.calendar.CalendarApplication.entity.Event;
 import com.calendar.CalendarApplication.entity.User;
 import com.calendar.CalendarApplication.repository.EventRepository;
@@ -14,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -70,6 +72,51 @@ class EventServiceTest {
     }
 
     @Test
-    void findEventsByUserId() {
+    @DisplayName("Finds user correctly by its id")
+    void findEventsByUserIdCase1() {
+        long userId = 1L;
+
+        Event event1 = new Event();
+        event1.setId(10L);
+        event1.setEventType(Event.EventType.COMPROMISE);
+        event1.setTitle("Reunião");
+        event1.setDescription("Com o time");
+        event1.setDate(new Date());
+        event1.setCompleted(true);
+
+        Event event2 = new Event();
+        event2.setId(20L);
+        event2.setEventType(Event.EventType.TASK);
+        event2.setTitle("Estudar");
+        event2.setDescription("Matemática");
+        event2.setDate(new Date());
+        event2.setCompleted(false);
+
+        List<Event> mockEvents = List.of(event1, event2);
+
+        when(eventRepository.findByUserId(userId)).thenReturn(mockEvents);
+
+        List<GetEventsDto> result = eventService.findEventsByUserId(userId);
+
+        assertEquals(2, result.size());
+        assertEquals("Reunião", result.get(0).title());
+        assertEquals("Estudar", result.get(1).title());
+        verify(eventRepository, times(1)).findByUserId(userId);
     }
+
+
+    @Test
+    @DisplayName("Does not find the user because the id is wrong")
+    void findEventsByUserIdCase2() {
+
+        long wrongUserId = 999L;
+        when(eventRepository.findByUserId(wrongUserId)).thenReturn(List.of());
+
+        List<GetEventsDto> result = eventService.findEventsByUserId(wrongUserId);
+
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+        verify(eventRepository, times(1)).findByUserId(wrongUserId);
+    }
+
 }
