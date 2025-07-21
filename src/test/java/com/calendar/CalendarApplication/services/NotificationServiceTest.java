fe6c8c8a9c38fe1067fe59f76d021fb756e417a1
gustavo <gugaxavier2005@gmail.com>
2025-07-21 18:55:53 -0300

    make tests for new user notification function of notification service

--- src/test/java/com/calendar/CalendarApplication/services/NotificationServiceTest.java
+++ src/test/java/com/calendar/CalendarApplication/services/NotificationServiceTest.java
@@ -1,7 +1,9 @@
 package com.calendar.CalendarApplication.services;
 
+import com.calendar.CalendarApplication.entity.Notification;
 import com.calendar.CalendarApplication.entity.User;
 import com.calendar.CalendarApplication.repository.NotificationRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,6 +13,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class NotificationServiceTest {
@@ -25,9 +29,31 @@ class NotificationServiceTest {
     SimpMessagingTemplate messagingTemplate;
 
     @Test
+    @DisplayName("Should act correctly when receiving the data")
     void newUserNotification() {
-//        Mockito.when(notificationRepository.findById(Mockito.anyLong())).thenReturn();
-//        notificationService.newUserNotification(new User());
+        User user = new User();
+        Notification expectedNotification = new Notification();
+        expectedNotification.setTitle("Bem Vindo à sua lista de Eventos e Tarefas");
+        expectedNotification.setDescription("Aqui você pode salvar seus eventos e tarefas que você tem marcados para servir de lembrete!");
+        expectedNotification.setUserId(user);
+        expectedNotification.setHasSeen(false);
+
+        when(notificationRepository.save(any(Notification.class))).thenAnswer(invocation -> {
+            Notification notificationToSave = invocation.getArgument(0);
+            notificationToSave.setId(1L);
+            return notificationToSave;
+        });
+
+        Notification result = notificationService.newUserNotification(user);
+
+        assertNotNull(result);
+        assertEquals("Bem Vindo à sua lista de Eventos e Tarefas", result.getTitle());
+        assertEquals("Aqui você pode salvar seus eventos e tarefas que você tem marcados para servir de lembrete!", result.getDescription());
+        assertEquals(user, result.getUser());
+        assertFalse(result.getHasSeen());
+        assertEquals(1L, result.getId());
+
+        verify(notificationRepository, times(1)).save(any(Notification.class));
     }
 
     @Test
